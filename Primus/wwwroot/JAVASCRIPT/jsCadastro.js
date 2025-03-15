ee9b1ddeba5e0702cf5ac2956f74e46e7df3e015
fr3ddykf <freddykf43@duck.com>
2025-03-14 21:15:53 -0300

    delete da versao antiga

--- Primus/wwwroot/JAVASCRIPT/jsCadastro.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("MostrarSenha").addEventListener("change", function() {
-    let senhaInput = document.getElementById("senha");
-    senhaInput.type = this.checked ? "text" : "password";
-});
