2348638fd96dd7632eee90d0a525d39f352b7491
QuilusS <luc.olivera0402@gmail.com>
2025-04-18 12:05:51 -0300

    Primus v1.9

---
+++ PRIMUS v1.9/JAVASCRIPT/jsCont.js
@@ -0,0 +1,122 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.nav-link'); // seleciona todos os elementos na página que têm a classe nav-link.
+    const currentFile = window.location.pathname.split('/').pop(); // Pega o nome do arquivo atual, como Notícias.html
+
+    console.log("Página atual: ", currentFile); // Exibe o nome do arquivo atual no console, como Notícias.html
+
+    links.forEach(link => {
+        const linkFile = link.getAttribute('href').split('/').pop(); // Pega o nome do arquivo do link
+
+        // Verifica se o nome do arquivo do link é igual ao nome da página atual
+        if (linkFile === currentFile) {
+            link.classList.add('active');
+        }
+
+        // Evento de clique para marcar o link como ativo
+        link.addEventListener('click', () => {
+            links.forEach(link => link.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+});
+
+//-------------------------------------//
+
+//Valida os campos do formulário
+function validarCampo(input, mensagemErro) {
+
+    if(input.value.trim() == ""){
+
+        mensagemErro.style.display = 'block';
+        return false;
+    }
+    else {
+        mensagemErro.style.display = 'none';
+        return true;
+    }
+}
+// Apos perder o foco dos inputs
+document.getElementById("name").addEventListener("blur", function() {
+    validarCampo(this, document.getElementById("MensagemErroCont"));
+});
+document.getElementById("emailAtendi").addEventListener("blur", function() {
+    validarCampo(this, document.getElementById("MensagemErro2Cont"));
+});
+document.getElementById("telefone").addEventListener("blur", function() {
+    validarCampo(this, document.getElementById("MensagemErro3"));
+});
+document.getElementById("caixatexto").addEventListener("blur", function() {
+    validarCampo(this, document.getElementById("MensagemErro4"));
+});
+document.getElementById("selecao").addEventListener("blur", function() {
+    validarCampo(this, document.getElementById("MensagemErro5"));
+});
+
+// Inicializa o EmailJS com seu userID
+    emailjs.init("DjX5EyyCmVafEbn0C");
+
+// Envia o formulário
+let form = document.getElementById('MyFormulario');
+let Aviso = document.getElementById('MyAviso');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var nome = document.getElementById("name");
+    var email = document.getElementById("emailAtendi");
+    var telefone = document.getElementById("telefone");
+    var caixatexto = document.getElementById("caixatexto");
+    var selecao = document.getElementById("selecao");
+
+    var nomeValido = validarCampo(nome, document.getElementById("MensagemErroCont"));
+    var emailValido = validarCampo(email, document.getElementById("MensagemErro2Cont"));
+    var telefoneValido = validarCampo(telefone, document.getElementById("MensagemErro3"));
+    var caixatextoValido = validarCampo(caixatexto, document.getElementById("MensagemErro4"));
+    var selecaoValido = validarCampo(selecao, document.getElementById("MensagemErro5"));
+
+
+    if (!nomeValido || !emailValido || !telefoneValido || !caixatextoValido || !selecaoValido) {
+        return;
+    }
+
+    emailjs.sendForm("service_7sujpvb", "template_f1q3xea", this)
+        .then(() => {
+            Aviso.textContent= "✔️ Mensagem enviada com sucesso!";
+            Aviso.className = "sucesso";
+            form.reset();
+        })
+        .catch(() => {
+            Aviso.textContent = "❌ Erro ao enviar. Tente novamente!";
+            Aviso.className = "erro";
+        })
+        .finally(() => {
+            Aviso.style.display = "block";
+            setTimeout(() => {
+                Aviso.style.display = "none"; // Esconde a mensagem após 5s
+            }, 5000);
+    });
+});
+// Apenas números no campo de telefone
+document.getElementById("telefone").addEventListener("input", function() {
+    this.value = this.value.replace(/\D/g, "");
+});
+
+//Efeito de Scroll
+document.addEventListener("DOMContentLoaded", () => {
+    const botaoSubir = document.getElementById('Subindo'); // Seleciona o div com o ID 'Subindo'
+    const valor = 150;
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > valor) {
+            botaoSubir.classList.add('show');
+
+        } else {
+            botaoSubir.classList.remove('show');
+
+        }
+    });
+});
+
+function Subir() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
---
+++ PRIMUS v1.9/PAGINAS/Produtos.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/PRIMUS/CSS/styleNavebar.css">
+    <link rel="stylesheet" href="../CSS/styleRodape.css">
+    <link rel="stylesheet" href="../CSS/styleProdut.css">
+    <link rel="shortcut icon" href="../IMGS/favicon.ico" type="image/x-icon">
+    <title>Produtos | Primu's Auto Peças</title>
+</head>
+<body>
+    <?php
+    session_start();
+    include '../PHP/navebar.php';
+    ?>
+
+    <main>
+        <div class="Barra">
+            <input type="text" id="btnPesquisar" placeholder="Pesquisar por produtos..." class="MyInput">
+        </div>
+        <br><br>
+        
+        <div id="productContainer" class="product-container">
+            <!-- Caixas de produto -->
+        </div>
+
+        <div class="EfeitoSubindo" id="Subindo">
+            <i class="fa-solid fa-arrow-up fa-beat"></i>
+            <button onclick="Subir()"><img src="../IMGS/roda.png" alt="roda"></button>
+        </div>
+
+    </main>
+
+    <script src="../JAVASCRIPT/jsProdut.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/d3aca5478d.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
